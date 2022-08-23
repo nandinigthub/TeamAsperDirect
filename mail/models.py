cd02e3e0da8ac569a516d18bc35ce77e0881a5ea
Nandini <nandinigattani9082@gmail.com>
2022-08-23 18:15:14 +0530

    ui update

--- mail/models.py
+++ mail/models.py
@@ -8,9 +8,3 @@ class Email(models.Model):
      subject = models.CharField(max_length=50)
      message = models.TextField(max_length=500)
      email = models.CharField(max_length=1000)
-
-   #   def __str__(self):
-   #      return self.email
-
-   #   def get_absolute_url(self):
-   #      return reverse('email/email.html', kwargs={'pk': self.pk})
--- mail/urls.py
+++ mail/urls.py
@@ -4,6 +4,4 @@ from . import views
 urlpatterns = [
     path('email/',views.email,name='email'),
     path('template/',views.template,name='template')
-
-    # +static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 ]
